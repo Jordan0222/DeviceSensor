f58a361141e6b4fe1f3621ac5fa10ed780e69bc4
asxz0 <asxz0121@gmail.com>
2022-05-10 12:55:11 +0800

    Light Sensor

---
+++ app/src/main/java/com/jordan/devicesensor/MeasurableSensor.kt
@@ -0,0 +1,17 @@
+package com.jordan.devicesensor
+
+abstract class MeasurableSensor(
+    protected val sensorType: Int
+) {
+
+    protected var onSensorValueChanged: ((List<Float>) -> Unit)? = null
+
+    abstract val doesSensorExist: Boolean
+
+    abstract fun startListening()
+    abstract fun stopListening()
+
+    fun setOnSensorValuesChangedListener(listener: (List<Float>) -> Unit) {
+        onSensorValueChanged = listener
+    }
+}
